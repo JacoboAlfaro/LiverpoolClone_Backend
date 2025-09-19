70fb45b9d34c6631211f9acfba2003fb45e7ad7c
JacoboAlfaro <jacobo.alfaro06@gmail.com>
2025-09-19 14:40:41 -0500

    Vercel deploy settings changes

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+// Creamos la app de Express
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: process.env.ORIGIN ?? 'http://localhost:3000',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 4000);
+  await app.init();
 }
+
+// Ejecutamos bootstrap
 bootstrap();
+
+// Exportamos el handler que Vercel necesita
+export default server;
